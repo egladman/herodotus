f47f015ef08da24417dd6591944ef906b916a407
Eli Gladman <eli.gladman@gmail.com>
2016-02-03 16:22:39 -0500

    utf-8 encoding

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var server = 'irc.freenode.net';
 var port = 6667
-var channel = '#helen_keller-testing';
+var channel = '#osuosc';
 var nick = 'helen_keller';
 
 var file = 'log.json';
@@ -9,23 +9,20 @@ var http = require('http');
 var irc = require('irc');
 var fs = require('fs');
 
-
 var client = new irc.Client(server, nick, {
+  autoRejoin: true,
   autoConnect: false,
   port: port,
   userName: nick,
-  secure: false
+  secure: false,
+  encoding: 'UTF-8'
 });
 
-
 function getDateTime() {
-  var date, sec, min, hour;
-
-  date = new Date();
-
-  sec  = date.getSeconds();
-  min  = date.getMinutes();
-  hour = date.getHours();
+  var date = new Date();
+  var sec  = date.getSeconds();
+  var min  = date.getMinutes();
+  var hour = date.getHours();
 
   sec = (sec < 10 ? "0" : "") + sec;
   min = (min < 10 ? "0" : "") + min;
@@ -34,9 +31,8 @@ function getDateTime() {
   return hour + ':' + min + ':' + sec;
 }
 
-
 client.connect(5, function(input) {
-  console.log("Connected");
+  // console.log("Connected");
   client.join(channel, function(input) {
     console.log("Joined " + channel);
   });
@@ -52,6 +48,6 @@ client.addListener('message', function (from, to, text) {
   array.push(item);
 
   fs.writeFile(file, '{' + array + '}', function() {
-    console.log("updated");
+    // console.log("updated");
   });
 });
